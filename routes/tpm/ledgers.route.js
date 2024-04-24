58d585f06f80cad4d844e1d8d842e8696c75a175
amru16 <raihanamru16@gmail.com>
2024-04-24 19:38:25 +0700

    ledgers route changes

--- routes/tpm/ledgers.route.js
+++ routes/tpm/ledgers.route.js
@@ -3,7 +3,7 @@ const { getLedgers, getDetail, addItemCheck, deleteItemCheck, editItemCheck } = 
 var router = express.Router();
 
 router.get('/search', getLedgers)
-router.get('/detail?:id', getDetail)
+router.get('/detail/search', getDetail)
 router.post('/addItemCheck', addItemCheck) //add item check parameter nya ledger id, lalu datanya yang akan di add disimpan di body
 router.put('/editItemCheck', editItemCheck) ////edit item check parameter nya ledger id, lalu datanya yang akan di add disimpan di body
 router.delete('/deleteItemCheck', deleteItemCheck) // //delete item check parameter nya ledger id, lalu datanya yang akan di add disimpan di body
