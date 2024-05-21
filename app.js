aab852b9242eb5d476d180d1caded1cabe9d6f2b
fajartc02 <fajartc02@gmail.com>
2024-05-21 17:33:33 +0700

    update fajar

--- app.js
+++ app.js
@@ -1,48 +1,45 @@
-require('dotenv').config()
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors')
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
 
-var routerV1 = require('./routes/v1/index');
+var routerV1 = require("./routes/v1/index");
 
-const { database } = require('./config/database');
+const { database } = require("./config/database");
 
 // NEED CRON TO MAKE SCHEDULE AUTOMATICALY DAILY FETCH
-const cronGeneratorSchedule = require('./functions/cronGeneratorSchedule');
-const cronCheckDelayStatus = require('./functions/cronCheckDelayStatus');
+const cronGeneratorSchedule = require("./functions/cronGeneratorSchedule");
+const cronCheckDelayStatus = require("./functions/cronCheckDelayStatus");
 
 async function init_start() {
-    await cronGeneratorSchedule()
-    await cronCheckDelayStatus()
+  await database.connect();
+  await cronGeneratorSchedule();
+  // await cronCheckDelayStatus()
 }
 
-init_start()
-
-
-
-database.connect()
-console.log('DB Connecttion:');
+init_start();
+console.log("DB Connecttion:");
 console.log({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    ssl: false
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  ssl: false,
 });
 console.log(`SERVER PORT: ${process.env.PORT}`);
 
 var app = express();
-app.use(cors())
+app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routerV1);
+app.use("/", routerV1);
 
 module.exports = app;
--- helpers/query.js
+++ helpers/query.js
@@ -1,162 +1,307 @@
-const { database } = require('../config/database')
-const moment = require('moment')
+const { database, databasePool } = require("../config/database");
+const moment = require("moment");
+
+const _defaultCallbackTrans = async (db = databasePool) => {};
 
 module.exports = {
-    queryGET: async (table, whereCond = false, cols = null, limit = null,) => {
-        return new Promise(async (resolve, reject) => {
-            let limit = ''
+  queryGET: async (table, whereCond = false, cols = null, limit = null) => {
+    return new Promise(async (resolve, reject) => {
+      let limit = "";
 
-            let selectedCols = '*'
-            if (cols) {
-                selectedCols = cols.join(',')
-            }
-            if (!whereCond) {
-                whereCond = ''
-            }
-            let q = `SELECT row_number() over()::INTEGER as no, ${selectedCols} FROM ${table} ${whereCond}`
-            // console.log(q);
-            await database.query(q)
-                .then((result) => {
-                    resolve(result.rows)
-                }).catch((err) => {
-                    reject(err)
-                });
+      let selectedCols = "*";
+      if (cols) {
+        selectedCols = cols.join(",");
+      }
+      if (!whereCond) {
+        whereCond = "";
+      }
+      let q = `SELECT row_number() over()::INTEGER as no, ${selectedCols} FROM ${table} ${whereCond}`;
+      // console.log(q);
+      await database
+        .query(q)
+        .then((result) => {
+          resolve(result.rows);
         })
-    },
-    queryPOST: async (table, data) => {
-        console.log(data);
-        return new Promise(async (resolve, reject) => {
-            let containerColumn = []
-            let containerValues = []
-            // handles data.length > 0
-            let isArray = data.length > 0
-            if (isArray) {
-                for (const key in data[0]) {
-                    containerColumn.push(key)
-                }
-                for (let i = 0; i < data.length; i++) {
-                    const element = data[i];
-                    let val = []
-                    for (const key in element) {
-                        val.push(`'${element[key]}'`)
-                    }
-                    containerValues.push(`(${val.join(',')})`)
-                }
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+  queryPOST: async (table, data) => {
+    console.log(data);
+    return new Promise(async (resolve, reject) => {
+      let containerColumn = [];
+      let containerValues = [];
+      // handles data.length > 0
+      let isArray = data.length > 0;
+      if (isArray) {
+        for (const key in data[0]) {
+          containerColumn.push(key);
+        }
+        for (let i = 0; i < data.length; i++) {
+          const element = data[i];
+          let val = [];
+          for (const key in element) {
+            val.push(`'${element[key]}'`);
+          }
+          containerValues.push(`(${val.join(",")})`);
+        }
+      } else {
+        for (const key in data) {
+          containerColumn.push(key);
+          containerValues.push(`'${data[key]}'`);
+        }
+      }
+
+      let q = `INSERT INTO ${table}(${containerColumn.join(
+        ","
+      )}) VALUES (${containerValues.join(",")}) RETURNING *`;
+      console.log(q);
+      await database
+        .query(q)
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((err) => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  },
+  queryBulkPOST: async (table, data) => {
+    return new Promise(async (resolve, reject) => {
+      let containerColumn = [];
+      let containerValues = [];
+      let mapBulkData = await data.map((item) => {
+        containerValues = [];
+        for (const key in item) {
+          if (key != "childs") {
+            if (item[key] || typeof item[key] === "number") {
+              console.log();
+              if (typeof item[key] == "object") {
+                containerValues.push(`'{${item[key].join(",")}}'`);
+              } else {
+                containerValues.push(`'${item[key]}'`);
+              }
             } else {
-                for (const key in data) {
-                    containerColumn.push(key)
-                    containerValues.push(`'${data[key]}'`)
-                }
+              containerValues.push(`NULL`);
             }
+          }
+        }
+        return `(${containerValues.join(",")})`;
+      });
+      for (const key in data[0]) {
+        containerColumn.push(key);
+      }
+      let q = `INSERT INTO ${table} (${containerColumn.join(
+        ","
+      )}) VALUES ${mapBulkData.join(",")} RETURNING *`;
+      // console.log(q);
+      await database
+        .query(q)
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((err) => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  },
+  queryPUT: async (table, data, whereCond = "") => {
+    return new Promise(async (resolve, reject) => {
+      let containerSetValues = [];
+      console.log(data);
+      for (const key in data) {
+        console.log(data[key] == 0);
+        if (
+          (data[key] && data[key] != "" && data[key] != "null") ||
+          data[key] == 0
+        ) {
+          containerSetValues.push(`${key} = '${data[key]}'`);
+        }
+      }
 
-            let q = `INSERT INTO ${table}(${containerColumn.join(',')}) VALUES (${containerValues.join(',')}) RETURNING *`
-            console.log(q);
-            await database.query(q)
-                .then((result) => {
-                    resolve(result)
-                }).catch((err) => {
-                    console.log(err);
-                    reject(err)
-                });
+      let q = `UPDATE ${table} SET ${containerSetValues.join(
+        ","
+      )} ${whereCond} RETURNING *`;
+      console.log(q);
+      await database
+        .query(q)
+        .then((result) => {
+          resolve(result);
         })
-    },
-    queryBulkPOST: async (table, data) => {
-        return new Promise(async (resolve, reject) => {
-            let containerColumn = []
-            let containerValues = []
-            let mapBulkData = await data.map(item => {
-                containerValues = []
-                for (const key in item) {
-                    if (key != 'childs') {
-                        if (item[key] || typeof item[key] === 'number') {
-                            console.log();
-                            if (typeof item[key] == 'object') {
-                                containerValues.push(`'{${item[key].join(',')}}'`)
-                            } else {
-                                containerValues.push(`'${item[key]}'`)
-                            }
-                        } else {
-                            containerValues.push(`NULL`)
-                        }
-                    }
-                }
-                return `(${containerValues.join(',')})`
-            })
-            for (const key in data[0]) {
-                containerColumn.push(key)
-            }
-            let q = `INSERT INTO ${table} (${containerColumn.join(',')}) VALUES ${mapBulkData.join(',')} RETURNING *`
-            // console.log(q);
-            await database.query(q)
-                .then((result) => {
-                    resolve(result)
-                }).catch((err) => {
-                    console.log(err);
-                    reject(err)
-                });
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+  queryDELETE: async (table, whereCond = "") => {
+    return new Promise(async (resolve, reject) => {
+      let q = `DELETE FROM ${table} ${whereCond}`;
+      await database
+        .query(q)
+        .then((result) => {
+          resolve(result);
         })
-    },
-    queryPUT: async (table, data, whereCond = '') => {
-        return new Promise(async (resolve, reject) => {
-            let containerSetValues = []
-            console.log(data);
-            for (const key in data) {
-                console.log(data[key] == 0);
-                if ((data[key]) && data[key] != '' && data[key] != 'null' || data[key] == 0) {
-                    containerSetValues.push(`${key} = '${data[key]}'`)
-                }
-            }
-
-            let q = `UPDATE ${table} SET ${containerSetValues.join(',')} ${whereCond} RETURNING *`
-            console.log(q);
-            await database.query(q)
-                .then((result) => {
-                    resolve(result)
-                }).catch((err) => {
-                    reject(err)
-                });
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+  queryCustom: async (sql) => {
+    return new Promise(async (resolve, reject) => {
+      let q = sql;
+      // console.log(q);
+      await database
+        .query(q)
+        .then((result) => {
+          resolve(result);
         })
-    },
-    queryDELETE: async (table, whereCond = '') => {
-        return new Promise(async (resolve, reject) => {
-            let q = `DELETE FROM ${table} ${whereCond}`
-            await database.query(q)
-                .then((result) => {
-                    resolve(result)
-                }).catch((err) => {
-                    reject(err)
-                });
-        })
-    },
-    queryCustom: async (sql) => {
-        return new Promise(async (resolve, reject) => {
-            let q = sql
-            // console.log(q);
-            await database.query(q)
-                .then((result) => {
-                    resolve(result)
-                }).catch((err) => {
-                    reject(err)
-                });
-        })
-    },
-    querySoftDELETE: async (table, whereCond = '', deleted_by) => {
-        console.log(deleted_by);
-        return new Promise(async (resolve, reject) => {
-            let deleted_dt = moment().format('YYYY-MM-DD');
-            let q = `UPDATE ${table} 
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+  querySoftDELETE: async (table, whereCond = "", deleted_by) => {
+    console.log(deleted_by);
+    return new Promise(async (resolve, reject) => {
+      let deleted_dt = moment().format("YYYY-MM-DD");
+      let q = `UPDATE ${table} 
                     SET 
                         deleted_by = '${deleted_by}',
                         deleted_dt = '${deleted_dt}'
                     where 
-                        ${whereCond}`
-            console.log(q);
-            await database.query(q)
-                .then((result) => {
-                    resolve(result)
-                }).catch((err) => {
-                    reject(err)
-                });
+                        ${whereCond}`;
+      console.log(q);
+      await database
+        .query(q)
+        .then((result) => {
+          resolve(result);
         })
-    },
-}
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+  queryTransaction: async (callback = _defaultCallbackTrans) => {
+    const db = await databasePool.connect();
+
+    const finish = async () => {
+      await db.query(`SET session_replication_role = 'origin'`);
+      db.release();
+    };
+
+    try {
+      await db.query(`SET session_replication_role = 'replica'`);
+      await db.query("BEGIN");
+
+      const r = await callback(db);
+      await db.query("COMMIT");
+      await finish();
+      return r;
+    } catch (error) {
+      //console.log('error transaction', error)
+      await db.query("ROLLBACK");
+      await finish();
+      throw error;
+    }
+  },
+  queryGetTransaction: async (
+    db,
+    table,
+    whereCond = false,
+    cols = null,
+    limit = null
+  ) => {
+    // async (table, whereCond = false, cols = null, limit = null) => {
+    return new Promise(async (resolve, reject) => {
+      let limit = "";
+
+      let selectedCols = "*";
+      if (cols) {
+        selectedCols = cols.join(",");
+      }
+      if (!whereCond) {
+        whereCond = "";
+      }
+      let q = `SELECT row_number() over()::INTEGER as no, ${selectedCols} FROM ${table} ${whereCond}`;
+      // console.log(q);
+      await db
+        .query(q)
+        .then((result) => {
+          resolve(result.rows);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+    // },
+  },
+  queryPostTransaction: async (dbPool, table, data) => {
+    return new Promise(async (resolve, reject) => {
+      let containerColumn = [];
+      let containerValues = [];
+      for (const key in data) {
+        containerColumn.push(key);
+
+        let value = data[key];
+        if (typeof value === "string" && value.includes("SELECT")) {
+          value = `${data[key]}`;
+        } else {
+          value = `'${data[key]}'`;
+        }
+
+        containerValues.push(
+          data[key] && data[key] != "null" ? `${value}` : "NULL"
+        );
+      }
+      let q = `INSERT INTO ${table}(${containerColumn.join(
+        ","
+      )}) VALUES (${containerValues.join(",")}) RETURNING *`;
+      console.log(q);
+      //logger(q)
+      await dbPool
+        .query(q)
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((err) => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  },
+  queryPutTransaction: async (dbPool, table, data, whereCond = "") => {
+    return new Promise(async (resolve, reject) => {
+      let containerSetValues = [];
+      for (const key in data) {
+        if (data[key] == "CURRENT_TIMESTAMP") {
+          containerSetValues.push(`${key} = CURRENT_TIMESTAMP`);
+        } else if (data[key] && data[key] != "null") {
+          let value = data[key];
+          if (typeof value === "string" && value.includes("select")) {
+            value = `${data[key]}`;
+          } else {
+            value = `'${data[key]}'`;
+          }
+
+          containerSetValues.push(`${key} = ${value}`);
+        }
+      }
+
+      let q = `UPDATE ${table} SET ${containerSetValues.join(
+        ","
+      )} ${whereCond} RETURNING *`;
+      console.log(q);
+      await dbPool
+        .query(q)
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+};
