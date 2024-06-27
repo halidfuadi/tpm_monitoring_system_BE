6eb664f9d602ff7b968cdbb5d3439f234fcca1c7
fajartc02 <fajartc02@gmail.com>
2024-06-27 21:06:25 +0700

    update for execution type number

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
 const pg = require("pg");
-pg.types.setTypeParser(1082, function (stringValue) {
-  return stringValue; //1082 for date type
+pg.types.setTypeParser(1082, function(stringValue) {
+    return stringValue; //1082 for date type
 });
-
+require("dotenv").config();
 const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: "tpm_backup",
-  port: process.env.DB_PORT,
-  host: process.env.DB_HOST,
-  ssl: false,
-  timezone: "Asia/Jakarta",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    ssl: false,
+    timezone: "Asia/Jakarta",
 };
 
 const database = new pg.Client(config);
@@ -19,14 +19,14 @@ const databasePool = new pg.Pool(config);
 
 const types = pg.types;
 types.setTypeParser(1114, (stringValue) => {
-  return stringValue; //1114 for time without timezone type
+    return stringValue; //1114 for time without timezone type
 });
 
 types.setTypeParser(1082, (stringValue) => {
-  return stringValue; //1082 for date type
+    return stringValue; //1082 for date type
 });
 
 module.exports = {
-  database,
-  databasePool,
+    database,
+    databasePool,
 };
--- controllers/tpm/execution.controllers.js
+++ controllers/tpm/execution.controllers.js
@@ -1,17 +1,22 @@
-const table = require('../../config/table')
-const { queryPOST, queryPUT, queryGET, queryCustom } = require('../../helpers/query')
-const queryHandler = require('../queryhandler.function')
-const response = require('../../helpers/response');
-const attrsUserUpdateData = require('../../helpers/addAttrsUserUpdateData');
-const getLastIdData = require('../../helpers/getLastIdData');
-const { v4 } = require('uuid');
-const idToUuid = require('../../helpers/idToUuid');
+const table = require("../../config/table");
+const {
+    queryPOST,
+    queryPUT,
+    queryGET,
+    queryCustom,
+} = require("../../helpers/query");
+const queryHandler = require("../queryhandler.function");
+const response = require("../../helpers/response");
+const attrsUserUpdateData = require("../../helpers/addAttrsUserUpdateData");
+const getLastIdData = require("../../helpers/getLastIdData");
+const { v4 } = require("uuid");
+const idToUuid = require("../../helpers/idToUuid");
 
 async function uuidToId(table, col, uuid) {
     console.log(`SELECT ${col} FROM ${table} WHERE uuid = '${uuid}'`);
     // `SELECT ${col} FROM ${table} WHERE uuid = '${uuid}'`
-    let rawId = await queryGET(table, `WHERE uuid = '${uuid}'`, [col])
-    return rawId[0][col]
+    let rawId = await queryGET(table, `WHERE uuid = '${uuid}'`, [col]);
+    return rawId[0][col];
 }
 
 async function execFinding(res, data) {
@@ -24,49 +29,88 @@ async function execFinding(res, data) {
     // VALUES
     //     (history_id, payload.finding.user_id, payload.finding.problem, payload.finding.action_plan, payload.finding.plan_check_dt)
     try {
-        const lastIdHistoryExec = await getLastIdData(table.tb_r_history_checks, 'history_check_id')
-        const lastIdFinding = await getLastIdData(table.tb_r_finding_checks, 'finding_id')
+        const lastIdHistoryExec = await getLastIdData(
+            table.tb_r_history_checks,
+            "history_check_id"
+        );
+        const lastIdFinding = await getLastIdData(
+            table.tb_r_finding_checks,
+            "finding_id"
+        );
 
-        const ledger_itemcheck_id = await uuidToId(table.tb_r_ledger_itemchecks, `ledger_itemcheck_id`, data.ledger_itemcheck_id)
-        const schedule_id = await uuidToId(table.tb_r_schedules, `schedule_id`, data.schedule_id)
+        const ledger_itemcheck_id = await uuidToId(
+            table.tb_r_ledger_itemchecks,
+            `ledger_itemcheck_id`,
+            data.ledger_itemcheck_id
+        );
+        const schedule_id = await uuidToId(
+            table.tb_r_schedules,
+            `schedule_id`,
+            data.schedule_id
+        );
 
-        const finding_user_id = await uuidToId(table.tb_m_users, `user_id`, data.finding.user_id)
+        const finding_user_id = await uuidToId(
+            table.tb_m_users,
+            `user_id`,
+            data.finding.user_id
+        );
 
         // Start: Map User Id Converted
-        const actual_user_ids = await data.actual_user_ids.map(async(act_user_id, i) => {
-            let conv_act_user_id = await uuidToId(table.tb_m_users, `user_id`, act_user_id)
-            let conv_plan_user_id = await uuidToId(table.tb_m_users, `user_id`, data.plan_user_ids[i])
-            return {
-                actual_user_id: conv_act_user_id,
-                user_id: conv_plan_user_id
+        const actual_user_ids = await data.actual_user_ids.map(
+            async(act_user_id, i) => {
+                let conv_act_user_id = await uuidToId(
+                    table.tb_m_users,
+                    `user_id`,
+                    act_user_id
+                );
+                let conv_plan_user_id = await uuidToId(
+                    table.tb_m_users,
+                    `user_id`,
+                    data.plan_user_ids[i]
+                );
+                return {
+                    actual_user_id: conv_act_user_id,
+                    user_id: conv_plan_user_id,
+                };
             }
-        })
-        const wait_users_ids = await Promise.all(actual_user_ids)
+        );
+        const wait_users_ids = await Promise.all(actual_user_ids);
 
         for (let idx = 0; idx < wait_users_ids.length; idx++) {
             const userIds = wait_users_ids[idx];
-            await queryPUT(table.tb_r_schedule_checker, userIds, `WHERE schedule_id = ${schedule_id} AND user_id = '${userIds.user_id}'`)
+            await queryPUT(
+                table.tb_r_schedule_checker,
+                userIds,
+                `WHERE schedule_id = ${schedule_id} AND user_id = '${userIds.user_id}'`
+            );
         }
         // End: Map User Id Converted
 
         const objSchedule = {
             status_id: 2, // for FINDING
-            actual_check_dt: data.actual_check_dt
-        }
+            actual_check_dt: data.actual_check_dt,
+        };
 
         const objCheckedExec = {
             history_check_id: lastIdHistoryExec,
             schedule_id: schedule_id,
             uuid: v4(),
-            checked_val: data.checked_val
-        }
+            checked_val: data.checked_val,
+        };
         const updtItemcheckLastDt = {
-            last_check_dt: data.actual_check_dt
-        }
+            last_check_dt: data.actual_check_dt,
+        };
 
-        await queryPUT(table.tb_r_schedules, objSchedule, `WHERE schedule_id = ${schedule_id}`)
+        await queryPUT(
+            table.tb_r_schedules,
+            objSchedule,
+            `WHERE schedule_id = ${schedule_id}`
+        );
 
-        let historyInstRes = await queryPOST(table.tb_r_history_checks, objCheckedExec)
+        let historyInstRes = await queryPOST(
+            table.tb_r_history_checks,
+            objCheckedExec
+        );
 
         const objFinding = {
             finding_id: lastIdFinding,
@@ -76,15 +120,19 @@ async function execFinding(res, data) {
             problem: data.finding.problem,
             action_plan: data.finding.action_plan,
             status_id: 0,
-            plan_check_dt: data.finding.plan_check_dt
-        }
+            plan_check_dt: data.finding.plan_check_dt,
+        };
 
-        await queryPOST(table.tb_r_finding_checks, objFinding)
-        await queryPUT(table.tb_r_ledger_itemchecks, updtItemcheckLastDt, `WHERE ledger_itemcheck_id = ${ledger_itemcheck_id}`)
-        return response.success(res, 'Success to execution schedule check')
+        await queryPOST(table.tb_r_finding_checks, objFinding);
+        await queryPUT(
+            table.tb_r_ledger_itemchecks,
+            updtItemcheckLastDt,
+            `WHERE ledger_itemcheck_id = ${ledger_itemcheck_id}`
+        );
+        return response.success(res, "Success to execution schedule check");
     } catch (error) {
         console.log(error);
-        return response.failed(res, 'Error to execution schedule check')
+        return response.failed(res, "Error to execution schedule check");
     }
 }
 
@@ -94,96 +142,155 @@ async function execNormal(res, data) {
     // INSERT INTO tb_r_history_checks(schedule_id, checked_val, uuid) VALUES(req.body.schedule_id, payload.checked_val, v4)
 
     try {
-        delete data.finding
-        const lastIdHistoryExec = await getLastIdData(table.tb_r_history_checks, 'history_check_id')
-        const schedule_id = await uuidToId(table.tb_r_schedules, `schedule_id`, data.schedule_id)
-        const ledger_itemcheck_id = await uuidToId(table.tb_r_ledger_itemchecks, `ledger_itemcheck_id`, data.ledger_itemcheck_id)
+        delete data.finding;
+        const lastIdHistoryExec = await getLastIdData(
+            table.tb_r_history_checks,
+            "history_check_id"
+        );
+        const schedule_id = await uuidToId(
+            table.tb_r_schedules,
+            `schedule_id`,
+            data.schedule_id
+        );
+        const ledger_itemcheck_id = await uuidToId(
+            table.tb_r_ledger_itemchecks,
+            `ledger_itemcheck_id`,
+            data.ledger_itemcheck_id
+        );
 
         // Start: Map User Id Converted
-        const actual_user_ids = await data.actual_user_ids.map(async(act_user_id, i) => {
-            let conv_act_user_id = await uuidToId(table.tb_m_users, `user_id`, act_user_id)
-            let conv_plan_user_id = await uuidToId(table.tb_m_users, `user_id`, data.plan_user_ids[i])
-            return {
-                actual_user_id: conv_act_user_id,
-                user_id: conv_plan_user_id
+        const actual_user_ids = await data.actual_user_ids.map(
+            async(act_user_id, i) => {
+                let conv_act_user_id = await uuidToId(
+                    table.tb_m_users,
+                    `user_id`,
+                    act_user_id
+                );
+                let conv_plan_user_id = await uuidToId(
+                    table.tb_m_users,
+                    `user_id`,
+                    data.plan_user_ids[i]
+                );
+                return {
+                    actual_user_id: conv_act_user_id,
+                    user_id: conv_plan_user_id,
+                };
             }
-        })
-        const wait_users_ids = await Promise.all(actual_user_ids)
+        );
+        const wait_users_ids = await Promise.all(actual_user_ids);
 
         for (let idx = 0; idx < wait_users_ids.length; idx++) {
             const userIds = wait_users_ids[idx];
-            await queryPUT(table.tb_r_schedule_checker, userIds, `WHERE schedule_id = ${schedule_id} AND user_id = '${userIds.user_id}'`)
+            await queryPUT(
+                table.tb_r_schedule_checker,
+                userIds,
+                `WHERE schedule_id = ${schedule_id} AND user_id = '${userIds.user_id}'`
+            );
         }
         // End: Map User Id Converted
 
         const objSchedule = {
             status_id: 4, // for DONE
             actual_check_dt: data.actual_check_dt,
-            actual_duration: data.actual_duration
-        }
+            actual_duration: data.actual_duration,
+        };
         const objCheckedExec = {
             history_check_id: lastIdHistoryExec,
             schedule_id: schedule_id,
             uuid: v4(),
-            checked_val: data.checked_val,            
-        }
-        if(data.actual_measurement){
-            objCheckedExec.act_measurement = +data.actual_measurement
+            checked_val: data.checked_val,
+        };
+        if (data.actual_measurement) {
+            objCheckedExec.act_measurement = +data.actual_measurement;
         }
         const updtItemcheckLastDt = {
-            last_check_dt: data.actual_check_dt      
-        }
+            last_check_dt: data.actual_check_dt,
+        };
         console.log(objCheckedExec);
-        await queryPUT(table.tb_r_schedules, objSchedule, `WHERE schedule_id = ${schedule_id}`)
-        await queryPOST(table.tb_r_history_checks, objCheckedExec)
-        await queryPUT(table.tb_r_ledger_itemchecks, updtItemcheckLastDt, `WHERE ledger_itemcheck_id = ${ledger_itemcheck_id}`)
-        return response.success(res, 'Success to execution schedule check')
+        await queryPUT(
+            table.tb_r_schedules,
+            objSchedule,
+            `WHERE schedule_id = ${schedule_id}`
+        );
+        await queryPOST(table.tb_r_history_checks, objCheckedExec);
+        await queryPUT(
+            table.tb_r_ledger_itemchecks,
+            updtItemcheckLastDt,
+            `WHERE ledger_itemcheck_id = ${ledger_itemcheck_id}`
+        );
+        return response.success(res, "Success to execution schedule check");
     } catch (error) {
         console.log(error);
-        return response.failed(res, 'Error to execution schedule check')
+        return response.failed(res, "Error to execution schedule check");
     }
 }
-
 
 module.exports = {
     getExecHistory: async(req, res) => {
         try {
-            let schedule_id = await uuidToId(table.tb_r_schedules, 'schedule_id', req.query.schedule_id)
-            req.query.schedule_id = schedule_id
-            let containerFilter = queryHandler(req.query)
-            containerFilter.length > 0 ? containerFilter = 'WHERE ' + containerFilter.join(" AND ") : containerFilter = ""
-            const scheduleData = await queryGET(table.tb_r_schedules, containerFilter, ['uuid as schedule_id', 'plan_check_dt', 'actual_check_dt', 'plan_duration', 'actual_duration'])
-            const executions = await queryGET(table.tb_r_history_checks, containerFilter)
-            const pic_check = await queryGET(table.tb_r_schedule_checker, containerFilter, ['actual_user_id ,user_id'])
-            scheduleData[0].execution = executions
+            let schedule_id = await uuidToId(
+                table.tb_r_schedules,
+                "schedule_id",
+                req.query.schedule_id
+            );
+            req.query.schedule_id = schedule_id;
+            let containerFilter = queryHandler(req.query);
+            containerFilter.length > 0 ?
+                (containerFilter = "WHERE " + containerFilter.join(" AND ")) :
+                (containerFilter = "");
+            const scheduleData = await queryGET(
+                table.tb_r_schedules,
+                containerFilter, [
+                    "uuid as schedule_id",
+                    "plan_check_dt",
+                    "actual_check_dt",
+                    "plan_duration",
+                    "actual_duration",
+                ]
+            );
+            const executions = await queryGET(
+                table.tb_r_history_checks,
+                containerFilter
+            );
+            const pic_check = await queryGET(
+                table.tb_r_schedule_checker,
+                containerFilter, ["actual_user_id ,user_id"]
+            );
+            scheduleData[0].execution = executions;
             const changesIdToUUID = await pic_check.map(async(user, i) => {
                 console.log(user);
-                let userData = await queryGET(table.tb_m_users, `WHERE user_id = ${user.user_id}`, ['uuid as user_id', 'user_nm', 'noreg'])
-                return userData[0]
-            })
-            scheduleData[0].pic_check = await Promise.all(changesIdToUUID)
-            response.success(res, 'success to get execution details', scheduleData)
+                let userData = await queryGET(
+                    table.tb_m_users,
+                    `WHERE user_id = ${user.user_id}`, ["uuid as user_id", "user_nm", "noreg"]
+                );
+                return userData[0];
+            });
+            scheduleData[0].pic_check = await Promise.all(changesIdToUUID);
+            response.success(res, "success to get execution details", scheduleData);
         } catch (error) {
             console.log(error);
-            response.failed(res, 'Error to get execution details')
+            response.failed(res, "Error to get execution details");
         }
     },
     addTpmExecution: async(req, res) => {
         try {
             console.log(req.body);
             if (+req.body.is_number) {
-                if (+req.body.checked_val >= +req.body.ok_val || +req.body.checked_val <= +req.body.ng_val) {
-                    execFinding(res, req.body)
+                if (+req.body.checked_val > +req.body.ok_val ||
+                    +req.body.checked_val < +req.body.ng_val
+                ) {
+                    execFinding(res, req.body);
                 } else {
-                    execNormal(res, req.body)
+                    execNormal(res, req.body);
                 }
             } else {
-                if (req.body.checked_val === req.body.ng_val) execFinding(res, req.body)
-                else execNormal(res, req.body)
+                if (req.body.checked_val === req.body.ng_val)
+                    execFinding(res, req.body);
+                else execNormal(res, req.body);
             }
         } catch (error) {
             console.log(error);
-            response.failed(res, 'Error to execution schedule check')
+            response.failed(res, "Error to execution schedule check");
         }
     },
-}
+};
